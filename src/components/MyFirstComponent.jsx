fe8f8946e4f565f1fdf79a8acf4dfcf637e06c55
Catalina Bejarano <catalina.bejarano@gmail.com>
2024-09-17 20:08:54 -0500

    My First Component

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,20 @@
+
+export const MyFirstComponent = () => {
+  return (
+    <>
+     <div>MyFirstComponent</div>
+
+     <h1>Temas de REACT</h1>
+     <ol>
+      <li>Componentes</li>
+      <li>Eventos</li>
+      <li>Estados Hooks</li>
+      <li>Props</li>
+     </ol>
+             
+    </>
+   
+  )
+}
+import React from 'react'
+
